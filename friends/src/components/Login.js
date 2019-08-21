b0f927249defef59c4dc3a82fe0b95528d63b493
cmstexas <crystal.m.soria@gmail.com>
2019-08-21 15:15:47 -0500

    bug fixes

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -2,12 +2,15 @@ import React from 'react';
 import axios from 'axios';
 
 class Login extends React.Component {
-  state = {
+constructor() {
+  super();
+  this.state = {
     credentials: {
       username: '',
       password: ''
     }
   };
+}
 
   handleChange = e => {
     this.setState({
@@ -24,6 +27,7 @@ class Login extends React.Component {
       .post('http://localhost:5000/api/login', this.state.credentials)
       .then(res => {
         localStorage.setItem('token', res.data.payload);
+        this.props.history.push("/protected");
       })
       .catch(err => console.log(err.response));
   };
